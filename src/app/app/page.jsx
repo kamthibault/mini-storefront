2ea4b46a9cc32555ba3e6588208c27a820ea6543
Kameron Thibault <kameronthibault@usf.edu>
2025-11-02 16:56:22 -0500

    display catalog

---
+++ src/app/app/page.jsx
@@ -0,0 +1,10 @@
+import ProductBrowser from "./components/catalog";
+
+export default function Page() {
+    return (
+        <main>
+            <h1>Browse Products</h1>
+            <ProductBrowser />
+        </main>
+    );
+}
