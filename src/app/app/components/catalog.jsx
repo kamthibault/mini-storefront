0c94a082e67072b56268c64f884c1eb381b1ca37
Kameron Thibault <kameronthibault@usf.edu>
2025-11-02 16:53:55 -0500

    catalog created

--- src/app/app/components/catalog.jsx
+++ src/app/app/components/catalog.jsx
@@ -3,3 +3,27 @@
 import React, {useEffect, useState} from `react`;
 import ProductList from `./productlist`;
 import StatusMessage from `./StatusMessage`;
+
+export default function ProductBrowser() {
+    const [products,setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch (`/api/products`)
+        .then(res => res.json())
+        .then(setProducts);
+    }, []);
+    
+
+    return (
+        <div>
+            <h2>Products</h2>
+            <ul>
+                {products.map(p => (
+                    <li key={p.id}>
+                        {p.name} - ${p.price}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
