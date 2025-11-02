4495e0c154617b19b709772bd3e9672963eb3de3
Kameron Thibault <kameronthibault@usf.edu>
2025-11-02 18:56:07 -0500

    Combine filters

--- src/app/app/page.jsx
+++ src/app/app/page.jsx
@@ -25,5 +25,31 @@ export default function Page() {
         .catch(err => setError(err.message))
         .finally(() => setLoading(false));
     }, []);
+
+    const handleCategoryFilter = (categoryFiltered) => {
+        setFiltered(categoryFiltered);
+    };
+    const handlePriceFilter = (priceFiltered) => {
+        setFiltered (priceFiltered);
+    };
+
+
+    return (
+        <main>
+            <h1>Browse Products</h1>
+        
+        {loading && <StatusMessage type="loading"/>}
+        {error && <StatusMessage type="error" message={error}/>}
+        {loading && !error && products.length == 0 && <StatusMessage type="empty"/>}
+
+        {!loading && !error && products.length>0 && (
+            <>
+            <CategoryFilter products={products} onFilter={handleCategoryFilter} />
+            <PriceFilter products={products} onFilter={handlePriceFilter}/>
+            <ProductList products={filtered} />
+            </>
+        )}
+        </main>
+    )
     
 }
